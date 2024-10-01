75ec770746aea2aaf89f9d651291588089d923f6
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-01 16:34:43 +0100

    hopefully final

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,16 @@
-// create your Media class:
+class Media{
+    static totalMediaCount = 0 
+    constructor(title, year, genre){
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        Media.totalMediaCount ++
+    }
+
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
+}
 
 // uncomment below to export it:
 module.exports = Media;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,28 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Music class:
+
+class Music extends Media{
+    constructor(title, year, genre, artist, length){
+        super(title, year, genre);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+
+    static shortestAlbum(arr){
+        let shortest = arr[0];
+        for(let i = 1; i < arr.length; i++){
+        if(arr[i].length < shortest.length){
+            shortest = arr[i]
+        }
+    }
+        return shortest
+    }
+}  
 
 // don't change below
 module.exports = Music;
